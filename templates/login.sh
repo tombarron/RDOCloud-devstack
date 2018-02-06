5ec67f4cd4393690319d1e9268de5e4d92637354
Tom Barron <tpb@dyncloud.net>
2018-02-06 16:04:50 -0500

    Clean up warnings and debug message

--- templates/login.sh
+++ templates/login.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 
-ssh -X -i ~/.ssh/id_rsa centos@{{ devstack_host_vm.server.public_v4 }}
+ssh -X \
+  -o StrictHostKeyChecking=no \
+  -o UserKnownHostsFile=/dev/null \
+  -i ~/.ssh/id_rsa centos@{{ devstack_host_vm.server.public_v4 }}
